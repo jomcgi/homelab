8947d79c6086baea7905f4ccc3c05dc5081d4dbb
Joe McGinley <116890464+jomcgi@users.noreply.github.com>
2025-03-22 01:40:43 +0000

    deploy uptime kuma

---
+++ services/github-action-uptime/api/actions_uptime.py
@@ -0,0 +1,30 @@
+from fastapi import Request
+import requests
+
+WORKFLOW_MAPPING = {
+    "Opentelemetry Collector": "6jsi0iFnCb",
+}
+
+UP_STATUSES = ["skipped", "cancelled", "success"]
+
+UPTIME_KUMA_URL = "http://uptime-kuma.uptime-kuma.svc.cluster.local:3001"
+
+def uptime(request: Request) -> None:
+    gh_payload = request.json()
+    assert "workflow_run" in gh_payload
+    workflow_run = gh_payload["workflow_run"]
+    if workflow_run["status"] != "completed":
+        return
+    if workflow_run["name"] not in WORKFLOW_MAPPING:
+        return  
+    url = f"{UPTIME_KUMA_URL}/api/push/{WORKFLOW_MAPPING[workflow_run['name']]}"
+    if workflow_run["conclusion"] not in UP_STATUSES:
+        message = f"Workflow {workflow_run['name']} failed - {workflow_run['url']}"
+        requests.get(url, params={  
+            "status": "down",
+            "message": message})   
+    else:
+        message = f"Workflow {workflow_run['name']} succeeded - {workflow_run['url']}"
+        requests.get(url, params={
+            "status": "up",
+            "message": message})
---
+++ services/github-action-uptime/api/main.py
@@ -0,0 +1,42 @@
+"""Github Actions Healthcheck API"""
+
+import logging
+
+import structlog
+import uvicorn
+from fastapi import FastAPI
+
+from health import HealthResponse
+from instrumentation import _instrument, configure_structlog
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from actions_uptime import uptime
+
+_configure_structlog()
+
+logging.basicConfig(level=logging.INFO)
+logger = structlog.get_logger(__name__)
+
+
+app = FastAPI(
+    on_startup=[_instrument, configure_structlog],
+)
+FastAPIInstrumentor.instrument_app(app, excluded_urls="health")
+
+
+app.add_api_route(
+    path="/health",
+    endpoint=get_health,
+    methods=["GET"],
+    status_code=200,
+    response_model=HealthResponse,
+)
+
+app.add_api_route(
+    path="/events",
+    endpoint=uptime,
+    methods=["POST"],
+    status_code=200,
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9090)
