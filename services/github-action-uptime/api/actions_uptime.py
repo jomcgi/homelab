6a7df736b09b66cb90988373025602b373978df8
Joe McGinley <116890464+jomcgi@users.noreply.github.com>
2025-03-22 16:04:25 +0000

    deploy uptime kuma

--- services/github-action-uptime/api/actions_uptime.py
+++ services/github-action-uptime/api/actions_uptime.py
@@ -1,14 +1,10 @@
 from fastapi import Request
 import requests
 
-WORKFLOW_MAPPING = {
-    "Opentelemetry Collector": "6jsi0iFnCb",
-}
-
 UP_STATUSES = ["skipped", "cancelled", "success"]
 
-# UPTIME_KUMA_URL = "http://uptime-kuma.uptime-kuma.svc.cluster.local:3001"
-UPTIME_KUMA_URL = "http://localhost:30333"
+UPTIME_KUMA_URL = "http://uptime-kuma.uptime-kuma.svc.cluster.local:3001"
+UPTIME_KUMA_URL = "http://host.docker.internal:30333"
 
 async def uptime(request: Request) -> None:
     gh_payload = await request.json()
@@ -17,9 +13,9 @@ async def uptime(request: Request) -> None:
     workflow_run = gh_payload["workflow_run"]
     if workflow_run["status"] != "completed":
         return
-    if workflow_run["name"] not in WORKFLOW_MAPPING:
+    if workflow_run["name"] != "Deploy Homelab":
         return  
-    url = f"{UPTIME_KUMA_URL}/api/push/{WORKFLOW_MAPPING[workflow_run['name']]}"
+    url = f"{UPTIME_KUMA_URL}/api/push/6jsi0iFnCb"
     if workflow_run["conclusion"] not in UP_STATUSES:
         message = f"Workflow {workflow_run['name']} failed - {workflow_run['url']}"
         requests.get(url, params={  
