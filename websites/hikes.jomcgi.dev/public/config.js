5ac3b3e38a7630be209200d28e905db82f8ed730
Joe <joe@jomcgi.dev>
2025-06-30 09:52:41 +0000

    wip

--- websites/hikes.jomcgi.dev/public/config.js
+++ websites/hikes.jomcgi.dev/public/config.js
@@ -5,8 +5,7 @@ const HIKES_CONFIG = {
     // R2 data URL with custom domain and bucket path
     dataUrl: 'https://hike-assets.jomcgi.dev/jomcgi-hikes/',
     
-    // Don't use local data - we have R2 working
-    useLocalData: false,
+    
     
     // Cache duration in minutes
     cacheMinutes: 10
