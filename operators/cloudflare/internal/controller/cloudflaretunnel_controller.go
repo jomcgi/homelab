57edcef348002bb151e9c80fed4fe7ef0c40c679
jomcgi <joe@jomcgi.dev>
2025-10-24 14:52:15 -0700

    fix(cloudflare-operator): remove unused intstr import

--- operators/cloudflare/internal/controller/cloudflaretunnel_controller.go
+++ operators/cloudflare/internal/controller/cloudflaretunnel_controller.go
@@ -32,7 +32,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
